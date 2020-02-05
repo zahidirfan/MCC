6ea423299a54f40cb4de02e17bc6f4695df88396
Zahid Irfan <zahid@qc-technologies.com>
2020-02-05 17:41:03 +0500

    Adding Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <div>
-<h1> Mezino Currency {props.name}</h1>
+    <div className='jumbotron'>
+      <h1> Mezino Currency {props.name}</h1>
+      <p>MCC helps you convert from one currency to another</p>
     </div>
   );
 }
